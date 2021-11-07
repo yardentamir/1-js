6a7d654f1a684e2d95489ce4bbb9d472fc834b47
yarentamir <yarden44441@gmail.com>
2021-11-07 13:06:00 +0200

    1.1

---
+++ 1.1/1.1.js
@@ -0,0 +1,24 @@
+let myStr = "Hello world";
+let myNum = 1;
+let myObj = {
+  key1: "value1",
+  key2: "value2",
+};
+let myBool = true;
+let myNull = null;
+let myUndefined = undefined;
+let mySymbol = Symbol("foo");
+
+console.log(
+  typeof myStr +
+    " " +
+    typeof myNum +
+    " " +
+    typeof myObj +
+    " " +
+    typeof myBool +
+    " " +
+    typeof myUndefined +
+    " " +
+    typeof mySymbol
+);
---
+++ 1.2/1.1-primative_data_types.js
@@ -0,0 +1,63 @@
+/*
+--Delete the wrong answers--
+
+1. Which of the following is/are strings?
+
+c) '4'
+
+2. Which of the following is/are numbers?
+
+a) 4
+b) 4.0
+d) -4
+
+3. Which of the following is/are booleans?
+
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+
+a) 151.2
+
+5. What is the result of "80" + 71.2?
+
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+
+b) string
+
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+// as a Number: let a = 1;
+// as a String: let mySt = "hello";
+// as a Boolean: let myBool = true;
+
+//declare a variable and reassign a value to it
+let b = 2;
+
+//create a variable and after that assign a string with its value being: He's got it!
+let myStr = "hello";
+myStr = "He's got it!";
+
+/*
+1. create a variable and assign a value on how much a restaurant bill is.
+const myBill = 100;
+2. create a variable and assign a value on how much tax they should pay.
+const myTax = 12.2;
+3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+ */
+
+
+
+// Round the number 50.6 to its nearest integer
+
+//Create a variable that is undefined

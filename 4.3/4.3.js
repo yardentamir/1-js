e39340c6e841fadf6515098f49126d81a030b7bd
yarentamir <yarden44441@gmail.com>
2021-11-08 17:48:36 +0200

    4.3

--- 4.3/4.3.js
+++ 4.3/4.3.js
@@ -1,5 +1,6 @@
 (function () {
   "use strict";
+
   const scoreTeam1 = () => (103 + 120 + 89) / 3;
   const scoreTeam2 = () => (123 + 94 + 116) / 3;
   const scoreTeam3 = () => (97 + 134 + 105) / 3;
@@ -7,7 +8,7 @@
   const whoWins = () => {
     if (scoreTeam1() > scoreTeam2() && scoreTeam1() > scoreTeam3())
       return "Team 1 wins with: " + scoreTeam1() + " points";
-    else if (scoreTeam1() == scoreTeam2() || scoreTeam1() == scoreTeam3())
+    else if (scoreTeam1() === scoreTeam2() || scoreTeam1() === scoreTeam3())
       return "draw";
     else if (scoreTeam2() > scoreTeam1() && scoreTeam2() > scoreTeam3())
       return "Team 2 wins with: " + scoreTeam2() + " points";
@@ -15,4 +16,15 @@
   };
 
   console.log(whoWins());
+
+  const myMax = Math.max(scoreTeam1(), scoreTeam2(), scoreTeam3());
+  if (myMax === scoreTeam1) {
+    console.log("team1 wins");
+  }
+  if (myMax === scoreTeam2) {
+    console.log("team2 wins");
+  }
+  if (myMax === scoreTeam3) {
+    console.log("team3 wins");
+  }
 })();
